07fc1df15f61099c954d3d3633678933591c89c0
NguyenCongQuoc <nguyencongquoc0904@gmail.com>
2020-12-20 16:51:24 +0700

    Add button Plus at bottom navigation

---
+++ app/src/main/java/com/example/doanjava/ui/add/AddViewModel.java
@@ -0,0 +1,21 @@
+package com.example.doanjava.ui.add;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import android.os.Bundle;
+
+public class AddViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AddViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is add fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
