fe0c0044b50a623ca5d1fa6bd693f25326686a9a
DESKTOP-32KD5B6\Hero <hung17dthja2@gmail.com>
2021-01-14 10:26:33 +0700

    ui char

--- app/src/main/java/com/example/doanjava/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/doanjava/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.doanjava.ui.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,22 +15,63 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.doanjava.R;
 
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
-
+    TextView tvR, tvPython, tvCPP, tvJava;
+    PieChart pieChart;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.txt_id);
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        PieChart pieChart;
+        tvR = root.findViewById(R.id.tvR);
+        tvPython = root.findViewById(R.id.tvPython);
+        tvCPP = root.findViewById(R.id.tvCPP);
+        tvJava = root.findViewById(R.id.tvJava);
+        pieChart = root.findViewById(R.id.piechart);
+
+        // Set the percentage of language used
+        tvR.setText(Integer.toString(40));
+        tvPython.setText(Integer.toString(30));
+        tvCPP.setText(Integer.toString(5));
+        tvJava.setText(Integer.toString(25));
+
+        // Set the data and color to the pie chart
+        pieChart.addPieSlice(
+                new PieModel(
+                        "R",
+                        Integer.parseInt(tvR.getText().toString()),
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Python",
+                        Integer.parseInt(tvPython.getText().toString()),
+                        Color.parseColor("#66BB6A")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "C++",
+                        Integer.parseInt(tvCPP.getText().toString()),
+                        Color.parseColor("#EF5350")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Java",
+                        Integer.parseInt(tvJava.getText().toString()),
+                        Color.parseColor("#29B6F6")));
+        // To animate the pie chart
+        pieChart.startAnimation();
         return root;
+
     }
 }
